23301a4cca2c600bb65290fa54fb61a427ad5aea
Danilo Mosca <danilo.mosca85@gmail.com>
2026-04-14 18:00:56 +0200

    Aggiunto esercizio della prima lezione del 13/04/2026
    Aggiunto esercizio "Hello World" della prima lezione del 13/04/2026

---
+++ lez.1-13-04-2026/Hellow.java
@@ -0,0 +1,10 @@
+// Il classico "Hello World" di inizio programmazione
+// Creo una classe di nome Hellow:
+public class Hellow {
+
+    // Il metodo main non è altro che il metodo principale, è il punto di accesso principale alla classe. E' la prima cosa che viene eseguita quando viene avviato il programma:
+    public static void main(String[] args) {
+        System.out.println("Hello world!");         // stampo a schermo una stringa
+    }
+
+}
