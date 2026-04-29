117d40772b425ade62e4bd37b000b9e2fd94588d
Danilo Mosca <danilo.mosca85@gmail.com>
2026-04-29 18:12:12 +0200

    added the text of the exercises
    added the text of the exercises

--- lez.13_29-04-2026-esempi-pattern/Esercizio1-Singleton/Main.java
+++ lez.13_29-04-2026-esempi-pattern/Esercizio1-Singleton/Main.java
@@ -1,37 +1,19 @@
-/* ESERCIZIO
-Design pattern ES Singleton
+/* ESERCIZIO: Design pattern ES Singleton
 
-Progetta un logger centralizzato che possa essere usato da qualsiasi parte del
-programma per scrivere messaggi di log su console. Il logger deve essere implementato
-seguendo il pattern Singleton.
-
+Progetta un logger centralizzato che possa essere usato da qualsiasi parte del programma per scrivere messaggi di log su console. Il logger deve essere implementato seguendo il pattern Singleton.
 
 Requisiti:
 
-
 Crea una classe Logger con:
-
-Un campo privato statico per contenere l’unica istanza (private static Logger
-istanza)
-
-Un costruttore privato
-
-Un metodo statico pubblico GetIstanza() che restituisce sempre la stessa
-istanza
-
-Un metodo ScriviMessaggio(string messaggio) che stampa il messaggio con data e
-ora
-
+    - Un campo privato statico per contenere l’unica istanza (private static Logger istanza)
+    - Un costruttore privato
+    - Un metodo statico pubblico GetIstanza() che restituisce sempre la stessa istanza
+    - Un metodo ScriviMessaggio(string messaggio) che stampa il messaggio con data e ora
 
 Nel Main:
-
-Richiama Logger.GetIstanza() da almeno due punti distinti del codice
-
-Usa ScriviMessaggio() per stampare messaggi diversi
-
-Dimostra che si tratta sempre della stessa istanza (es. confrontando i
-riferimenti)
-
+    - Richiama Logger.GetIstanza() da almeno due punti distinti del codice
+    - Usa ScriviMessaggio() per stampare messaggi diversi
+    - Dimostra che si tratta sempre della stessa istanza (es. confrontando i riferimenti)
 */
 
 public class Main {
